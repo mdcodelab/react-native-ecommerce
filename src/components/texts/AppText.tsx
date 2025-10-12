d145384e4637a47f0da3e3fddf7972db834a55e0
Mihaela Diaconu <contact.mihaela.office@gmail.com>
2025-10-12 10:56:29 +0300

    ApPtext

---
+++ src/components/texts/AppText.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Text, StyleSheet, TextProps, StyleProp, TextStyle } from 'react-native';
+import {s} from 'react-native-size-matters';
+
+interface AppTextProps extends TextProps {
+  children?: React.ReactNode;
+  style?: StyleProp<TextStyle>;
+  variant?: 'bold' | 'medium';
+}
+
+function AppText({ children, style, variant = 'bold', ...rest }: AppTextProps) {
+  return (
+    <Text style={[Styles[variant], style]} {...rest}>
+      {children}
+    </Text>
+  );
+}
+
+const Styles = StyleSheet.create({
+bold: {
+  fontSize: s(18),
+  color: "#000"
+},
+medium: {
+  fontSize: s(16),  
+  color: "#000"
+}
+});
+    
+
+export default AppText;
+
+
+
+
+
+
