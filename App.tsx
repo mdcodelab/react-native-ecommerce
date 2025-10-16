d4a5e18c9b26c7ecf408b1c2c3aa8ef1ac2f8300
Mihaela Diaconu <contact.mihaela.office@gmail.com>
2025-10-16 21:48:58 +0300

    Flash Message

--- App.tsx
+++ App.tsx
@@ -1,15 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
 import AppText from './src/components/texts/AppText';
 import AppSaveView from './src/components/views/AppSaveView';
+import FlashMessage, { showMessage } from 'react-native-flash-message';
 
 export default function App() {
   return (
+    <>
+    <FlashMessage position="top" />
     <AppSaveView style={styles.container}>
-      <AppText variant="medium">Hello World!</AppText>
+      <AppText
+        variant="medium"
+        onPress={() => showMessage({ message: 'Hello World!', type: 'info' })}
+      >
+        Hello World!
+      </AppText>
       <AppText variant="bold">Hello World!</AppText>
       <StatusBar style="auto" />
     </AppSaveView>
+    </>
   );
 }
 
